a5cddcfba3548157b61a2ba218e840546232f42b
SOUGATA SAHA <sahasougata820@gmail.com>
2026-04-22 00:25:20 +0530

    refactor frontend init

---
+++ spendfront/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-950">
+      <h1 className="text-4xl font-bold text-white">SpendWise</h1>
+    </div>
+  )
+}
+
+export default App
